eef8ddd616035697447098cb27875796b3c00830
Hancel.Lin <imlinhanchao@gmail.com>
2023-03-08 22:44:20 +0800

    🐞 fix: 移除默认文字，添加 url 默认值

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ let app = new Vue({
     isDrag: false,
     image: '',
     repeat: true,
-    text: '仅供租房备案使用，他用无效',
+    text: '',
     angle: 45,
     style: {
       color: '#ffffff',
@@ -46,9 +46,70 @@ let app = new Vue({
     download() {
       domtoimage.toBlob(document.getElementById('result')).then(function (blob) {
         window.saveAs(blob, 'result.png');
-    });
+      });
+    },
+    getDefault() {
+      // 从 location search 中获取默认值
+      let search = location.search;
+      if (search) {
+        let params = search.slice(1).split('&');
+        params.forEach((item) => {
+          let [key, value] = item.split('=');
+          if (key === 'text') {
+            this.text = decodeURIComponent(value);
+          } else if (key === 'color') {
+            this.style.color = value;
+          } else if (key === 'size') {
+            this.style.fontSize = value;
+          } else if (key === 'opacity') {
+            this.style.opacity = value / 100;
+          } else if (key === 'weight') {
+            this.style.fontWeight = value === 'bold';
+          } else if (key === 'angle') {
+            this.angle = value;
+          } else if (key === 'repeat') {
+            this.repeat = value === '1';
+          } else if (key === 'src') {
+            this.image = decodeURIComponent(value);
+          }
+        })
+      }
+    },
+    dragenterHandler(e) {
+      this.isDrag = true;
+      e.preventDefault();
+    },
+    dragleaveHandler(e) {
+      this.isDrag = false;
+    },
+    dragoverHandler(e) {
+      this.isDrag = true;
+      e.preventDefault();
+    },
+    dropHandler(e) {
+      this.isDrag = false;
+      let files = e.dataTransfer.files;
+      Array.from(files).forEach(f => {
+        if (f.type.indexOf('image') < 0) return;
+        this.previewFile(f);
+      })
+      e.preventDefault();
+    },
+    pasteHandle(e) {
+      let items = event.clipboardData && event.clipboardData.items;
+      Array.from(items).forEach(f => {
+        if (f.type.indexOf('image') < 0) return;
+        let file = f.getAsFile();
+        this.previewFile(file);
+      })
     }
   },
   mounted: function () {
+    this.$refs['drag-area'].addEventListener('dragenter', this.dragenterHandler, false);
+    this.$refs['drag-area'].addEventListener('dragleave', this.dragleaveHandler, false);
+    this.$refs['drag-area'].addEventListener('dragover', this.dragoverHandler, false);
+    this.$refs['drag-area'].addEventListener('drop', this.dropHandler, false);
+    document.addEventListener('paste', this.pasteHandle, false);
+    this.getDefault();
   }
 })
